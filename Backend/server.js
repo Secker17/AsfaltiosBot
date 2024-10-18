2118f67f7db7a22580c6aa92e0c952a5463b7d4e
Secker17 <martin@secker.no>
2024-10-18 14:05:53 +0200

    new

--- Backend/server.js
+++ Backend/server.js
@@ -112,5 +112,5 @@ app.get('/api/chat/messages/:chatId', (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
